f7680890783e31118135896faa7dbba7c0166a51
gnat <hellunatalia@gmail.com>
2025-10-24 17:02:27 -0700

    worked on base styling as well as some js

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'music/nicetoknowyou.mp3',
+        artist: 'artist 1',
+        cover: 'images/fancythat-cover'
+    },
+    {
+        name: 'song 2',
+        path: 'music/romeo.mp3',
+        artist: 'artist 2',
+        cover: 'images/fancythat-cover'
+    },
+    {
+        name: 'song 3',
+        path: 'music/mirrors.mp3',
+        artist: 'artist 3',
+        cover: 'images/sailingsouls-cover'
+    },
+    {
+        name: 'song 4',
+        path: 'music/stay.mp3',
+        artist: 'artist 4',
+        cover: 'images/stay.mp3'
+    }
+]
