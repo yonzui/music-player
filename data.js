5889d880a49fe5be042b24cc8bbc52f9ef17f6a0
gnat <hellunatalia@gmail.com>
2025-10-27 14:31:08 -0700

    styling and code updates for playing music

--- data.js
+++ data.js
@@ -1,26 +1,74 @@
 let songs = [
     {
-        name: 'song 1',
+        name: 'nice to know you',
         path: 'music/nicetoknowyou.mp3',
-        artist: 'artist 1',
-        cover: 'images/fancythat-cover'
+        artist: 'pinkpantheress',
+        cover: 'images/fancythat-cover.jpg'
     },
     {
-        name: 'song 2',
+        name: 'romeo',
         path: 'music/romeo.mp3',
-        artist: 'artist 2',
-        cover: 'images/fancythat-cover'
+        artist: 'pinkpantheress',
+        cover: 'images/fancythat-cover.jpg'
     },
     {
-        name: 'song 3',
+        name: 'snap my finger (ft. pinkpantheress)',
+        path: 'music/snapmyfinger.mp3',
+        artist: 'kaytranada',
+        cover: 'images/kaytranada-cover.jpg'
+    },
+    {
+        name: 'mirrors',
         path: 'music/mirrors.mp3',
-        artist: 'artist 3',
-        cover: 'images/sailingsouls-cover'
+        artist: 'jhené aiko',
+        cover: 'images/sailingsouls-cover.jpg'
+    },
+        {
+        name: 'living room flow',
+        path: 'music/livingroomflow.mp3',
+        artist: 'jhené aiko',
+        cover: 'images/sailingsouls-cover.jpg'
     },
     {
-        name: 'song 4',
+        name: 'wading',
+        path: 'music/wading.mp3',
+        artist: 'jhené aiko',
+        cover: 'images/souledout-cover.jpg'
+    },
+    {
+        name: 'stay (ft. dana williams)',
         path: 'music/stay.mp3',
-        artist: 'artist 4',
-        cover: 'images/stay.mp3'
+        artist: 'the aston shuffle, fabich',
+        cover: 'images/stay-cover.jpg'
+    },
+    {
+        name: 'club classics (ft. bb trickz)',
+        path: 'music/clubclassics.mp3',
+        artist: 'charli xcx',
+        cover: 'images/brat-cover.jpg'
+    },
+    {
+        name: 'xy',
+        path: 'music/xy.mp3',
+        artist: 'feng',
+        cover: 'images/wtf-cover.jpg'
+    },
+    {
+        name: 'butterfly',
+        path: 'music/butterfly.mp3',
+        artist: 'bts',
+        cover: 'images/butterfly-cover.jpg'
+    },
+    {
+        name: 'ditto',
+        path: 'music/ditto.mp3',
+        artist: 'newjeans',
+        cover: 'images/ditto-cover.jpg'
+    },
+    {
+        name: 'notice (ft. toriena)',
+        path: 'music/notice.mp3',
+        artist: 'moe shop',
+        cover: 'images/notice-cover.jpg'
     }
 ]
