6ca9b0227505254272b9c4a4445a67f1fa63ed8d
gnat <hellunatalia@gmail.com>
2025-10-27 14:30:49 -0700

    styling and code updates for playing music

--- app.js
+++ app.js
@@ -61,10 +61,38 @@ const formatTime = (time) => {
 setInterval(() => {
     seekBar.value = music.currentTime;
     currentTime.innerHTML = formatTime(music.currentTime);
+    if(Math.floor(music.currentTime) = Math.floor(seekBar.max)){
+        forwardBtn.click();
+    }
 }, 500)
 
 seekBar.addEventListener('change', () => {
     music.currentTime = seekBar.value;
 })
 
+const playMusic = () => {
+    music.play();
+    playBtn.classList.remove('pause');
+    disk.classList.add('play');
+}
 
+
+forwardBtn.addEventListener('click', () => {
+    if(currentMusic >= songs.length - 1){
+        currentMusic = 0;
+    } else{
+        currentMusic++
+    }
+    setMusic(currentMusic);
+    playMusic();
+})
+
+backwardBtn.addEventListener('click', () => {
+    if(currentMusic <= 0){
+        currentMusic = songs.length - 1;
+    } else {
+        currentMusic--;
+    }
+    setMusic(currentMusic);
+    playMusic();
+})
